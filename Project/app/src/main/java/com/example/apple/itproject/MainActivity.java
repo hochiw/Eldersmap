2812b47e70fd673d0d677af76ec12bb361b7e558
Chengeng Liu <c.liu75@student.unimelb.edu.au>
2018-09-01 19:08:57 +1000

    Add Marker

--- Project/app/src/main/java/com/example/apple/itproject/MainActivity.java
+++ Project/app/src/main/java/com/example/apple/itproject/MainActivity.java
@@ -1,14 +1,48 @@
 package com.example.apple.itproject;
 
+import android.graphics.PointF;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.gson.JsonElement;
+import com.mapbox.geojson.Feature;
+import com.mapbox.mapboxsdk.Mapbox;
+import com.mapbox.mapboxsdk.geometry.LatLng;
+import com.mapbox.mapboxsdk.maps.MapView;
+import com.example.apple.itproject.R;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
 import com.mapbox.mapboxsdk.Mapbox;
 import com.mapbox.mapboxsdk.maps.MapView;
+import com.mapbox.mapboxsdk.maps.MapboxMap;
+import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
+import com.mapbox.mapboxsdk.style.layers.PropertyFactory;
+import com.mapbox.mapboxsdk.style.layers.SymbolLayer;
+import com.mapbox.mapboxsdk.style.sources.GeoJsonSource;
+import com.mapbox.geojson.Feature;
+import com.mapbox.geojson.FeatureCollection;
+import com.mapbox.geojson.Point;
+import com.mapbox.geojson.MultiPoint;
 
 
-public class MainActivity extends AppCompatActivity {
+
+import java.util.List;
+import java.util.Map;
+import java.util.ArrayList;
+
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
     private MapView mapView;
+    private static final String MARKER_SOURCE = "markers-source";
+    private static final String MARKER_STYLE_LAYER = "markers-style-layer";
+    private static final String MARKER_IMAGE = "custom-marker";
+    private MapboxMap mapboxMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,8 +51,48 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         mapView = (MapView) findViewById(R.id.mapView);
         mapView.onCreate(savedInstanceState);
+        mapView.getMapAsync(this);
 
     }
+
+
+    @Override
+    public void onMapReady(MapboxMap mapboxMap) {
+        MainActivity.this.mapboxMap = mapboxMap;
+        /* Image: An image is loaded and added to the map. */
+        Bitmap icon = BitmapFactory.decodeResource(
+                MainActivity.this.getResources(), R.drawable.custom_marker);
+        mapboxMap.addImage(MARKER_IMAGE, icon);
+        addMarkers();
+    }
+
+
+
+
+
+
+    private void addMarkers() {
+        //final Point origin = Point.fromLngLat(new double[] {10,10});
+        List<Feature> features = new ArrayList<>();
+        /* Source: A data source specifies the geographic coordinate where the image marker gets placed. */
+        features.add(Feature.fromGeometry(Point.fromLngLat(174.00212, -36.88686)));
+        FeatureCollection featureCollection = FeatureCollection.fromFeatures(features);
+        GeoJsonSource source = new GeoJsonSource(MARKER_SOURCE, featureCollection);
+
+        mapboxMap.addSource(source);
+        /* Style layer: A style layer ties together the source and image and specifies how they are displayed on the map. */
+        SymbolLayer markerStyleLayer = new SymbolLayer(MARKER_STYLE_LAYER, MARKER_SOURCE)
+                .withProperties(
+                        PropertyFactory.iconAllowOverlap(true),
+                        PropertyFactory.iconImage(MARKER_IMAGE)
+                );
+        mapboxMap.addLayer(markerStyleLayer);
+    }
+
+
+
+
+
 
     @Override
     public void onStart() {

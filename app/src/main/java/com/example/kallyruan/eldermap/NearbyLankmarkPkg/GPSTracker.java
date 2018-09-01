c071ce55e6979f6376b2ed6809e874df63b0423b
TakoYaki <rico5504@gmail.com>
2018-09-01 19:08:53 +1000

    Added GPS Tracker

---
+++ app/src/main/java/com/example/kallyruan/eldermap/NearbyLankmarkPkg/GPSTracker.java
@@ -0,0 +1,83 @@
+package com.example.kallyruan.eldermap.NearbyLankmarkPkg;
+
+import android.Manifest;
+import android.location.Criteria;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
+
+
+public class GPSTracker implements LocationListener {
+
+
+    private boolean GPSEnabled = false;
+    private boolean networkEnabled = false;
+    private Context context;
+    private LocationManager locationManager;
+    private android.location.Location location;
+
+    public GPSTracker(Context context) {
+        this.context = context;
+    }
+
+    public Location getLocation() {
+
+            locationManager = (LocationManager) context.getSystemService(context.LOCATION_SERVICE);
+            GPSEnabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+            networkEnabled = locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+
+        if (ContextCompat.checkSelfPermission( context, android.Manifest.permission.ACCESS_FINE_LOCATION ) == PackageManager.PERMISSION_GRANTED &&
+                    ContextCompat.checkSelfPermission( context, Manifest.permission.ACCESS_COARSE_LOCATION ) == PackageManager.PERMISSION_GRANTED) {
+
+
+            if (GPSEnabled) {
+                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,0,0,this);
+                if (locationManager != null) {
+                    location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                    if (location != null) {
+                        return new com.example.kallyruan.eldermap.NearbyLankmarkPkg.Location(
+                                location.getLatitude(),location.getLongitude()
+                         );
+                        }
+                    }
+                } else if (networkEnabled) {
+                locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER,0,0,this);
+                if (locationManager != null) {
+                    location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+                    if (location != null) {
+                        return new com.example.kallyruan.eldermap.NearbyLankmarkPkg.Location(
+                                location.getLatitude(),location.getLongitude()
+                            );
+                        }
+                    }
+                }
+            }
+        return null;
+
+
+    }
+
+    @Override
+    public void onLocationChanged(android.location.Location location) {
+    }
+
+    @Override
+    public void onStatusChanged(String s, int i, Bundle bundle) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String s) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String s) {
+
+    }
+}

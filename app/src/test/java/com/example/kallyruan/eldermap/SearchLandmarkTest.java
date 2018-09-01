03a1b68f279d98017b1692766a0b45a131bd5071
Kah Chun Lee <kahl2@student.unimelb.edu.au>
2018-09-01 18:40:23 +1000

    Unit test initial
    Just defined structure. Will continue in wed

---
+++ app/src/test/java/com/example/kallyruan/eldermap/SearchLandmarkTest.java
@@ -0,0 +1,45 @@
+package com.example.kallyruan.eldermap;
+
+import com.example.kallyruan.eldermap.NearbyLankmarkPkg.Landmark;
+import com.example.kallyruan.eldermap.NearbyLankmarkPkg.LandmarkListActivity;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import static org.mockito.Mockito.*;
+/**
+ * Created by Kah Chun on 01/09/2018.
+ * Adapted from William's Postage Calculator unit test example.
+ */
+
+public class SearchLandmarkTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void testLandmarkSelection() throws Exception {
+
+        // pass mock objects as dependency to constructor
+        LandmarkListActivity landmarkListActivity = mock(LandmarkListActivity.class);
+        
+
+        // configure mock expected interaction
+
+
+        // call method being tested
+
+
+        // check if mock had expected interaction
+
+        // assert results
+
+    }
+
+}

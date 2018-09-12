6be097a7d02b92abcfe45902f6bd185f5e03322d
kallyrhy <kally.rhy@gmail.com>
2018-09-12 18:13:20 +1000

    merged with latest rico123 branch

--- app/src/main/java/com/example/kallyruan/eldermap/NearbyLankmarkPkg/LandmarkListActivity.java
+++ app/src/main/java/com/example/kallyruan/eldermap/NearbyLankmarkPkg/LandmarkListActivity.java
@@ -42,6 +42,9 @@ public class LandmarkListActivity extends Activity {
     private boolean serviceAlive = false;
     private GPSTracker gps;
 
+    public static String destination;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -68,6 +71,7 @@ public class LandmarkListActivity extends Activity {
         @Override
         public void onServiceDisconnected(ComponentName name) {
             serviceAlive = false;
+
         }
 
         @Override
@@ -77,11 +81,12 @@ public class LandmarkListActivity extends Activity {
             gps = mBinder.getInstance();
             serviceAlive = true;
             try {
-                showLandmarkList();
+                showLandmarkList(destination);
             } catch (Exception e) {
                 e.printStackTrace();
             }
         }
+
     };
 
     //check whether users click on a landmark
@@ -183,13 +188,15 @@ public class LandmarkListActivity extends Activity {
     /**
      * Shows the list of nearby landmarks
      */
-    public void showLandmarkList() throws  JSONException, ExecutionException, InterruptedException {
+    public void showLandmarkList(String targetLoc) throws  JSONException, ExecutionException, InterruptedException {
         ListView listView = (ListView) findViewById(R.id.landmark_list);
         ArrayList<Landmark> list = new ArrayList<>();
         //Data Input
         Location userLoc = gps.getLoc();
-        JSONObject userData = JSONFactory.userDataJSONMaker(userLoc, "");
-        Log.d("tests", userLoc.getLatitude().toString());
+
+        JSONObject userData = JSONFactory.userDataJSONMaker(userLoc, targetLoc);
+        Log.d("Uer ", userLoc.getLatitude().toString());
+
         //ArrayList<Landmark> list = searchAlg.filterList(JSONFactory.parseJSON("http://eldersmapapi.herokuapp.com/api/search"));
         JSONObject result = new HTTPPostRequest("http://eldersmapapi.herokuapp.com/api/search").execute(userData).get();
         if(result.get("status").toString().equals("OK")){
@@ -199,5 +206,12 @@ public class LandmarkListActivity extends Activity {
         LandmarkListAdapter adapter = new LandmarkListAdapter(this, list);
         listView.setAdapter(adapter);
     }
+
+    // curLatitude curLongtitude
+
+    // desLatitude desLongtitude
+
+    //http://eldersmapapi.herokuapp.com/api/route.
+
 }
 

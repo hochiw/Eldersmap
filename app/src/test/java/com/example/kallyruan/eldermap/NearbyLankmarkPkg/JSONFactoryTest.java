7339de2b031aec5f454b7c543f4bcdfc00f02b0f
Chengeng Liu <c.liu75@student.unimelb.edu.au>
2018-10-06 17:31:13 +1000

    JSONFactory test finished

--- app/src/test/java/com/example/kallyruan/eldermap/NearbyLankmarkPkg/JSONFactoryTest.java
+++ app/src/test/java/com/example/kallyruan/eldermap/NearbyLankmarkPkg/JSONFactoryTest.java
@@ -6,6 +6,8 @@ import org.json.JSONObject;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
+import static org.powermock.api.easymock.PowerMock.replay;
+import static org.powermock.api.easymock.PowerMock.verify;
 
 import org.mockito.BDDMockito;
 import org.powermock.api.easymock.PowerMock;
@@ -23,34 +25,55 @@ import org.powermock.modules.junit4.PowerMockRunner;
 
 
 @RunWith(PowerMockRunner.class)
-@PrepareForTest({Location.class,JSONFactory.class})
+@PrepareForTest({JSONFactory.class, JSONObject.class})
 public class JSONFactoryTest {
 
     @Mock
     Location userLoc;
 
-    @Mock
-    String landmarkType;
+    String landmarkType = "HelloWorld";
 
-    @InjectMocks
-    JSONFactory factory;
+    @Before
+    public void setup() throws  Exception{
+        PowerMockito.mockStatic(JSONFactory.class);
+
+        PowerMockito.when(JSONFactory.userDataJSONMaker(userLoc,landmarkType)).thenReturn(new JSONObject());
+        userLoc = Mockito.mock(Location.class);
+
+        Mockito.when(userLoc.getLatitude()).thenReturn(0.0);
+        Mockito.when(userLoc.getLongitude()).thenReturn(0.0);
+
+    }
 
     @Test
     public void userDataJSONMaker() throws  Exception{
-        JSONObject obj;
-        PowerMock.mockStatic(JSONFactory.class);
 
-        BDDMockito.given(JSONFactory.userDataJSONMaker(userLoc,landmarkType)).
-                willReturn(new JSONObject());
-
-        Mockito.when(userLoc.getLatitude()).thenReturn(0.0);
-        //JSONFactory.userDataJSONMaker(userLoc,landmarkType);
+        //JSONObject jsonObject = Mockito.mock(JSONObject.class);
+        //Alternately
+        JSONObject jsonObject = PowerMock.createMock(JSONObject.class);
 
 
-        //Mockito.when(userLoc.getLongitude()).thenReturn(0.0);
+//        userLoc.setLatitude(0.0);
+//        userLoc.setLongitude(0.0);
+        //TODO: 10.6 Saturday
+        // Verify construction of object.
+        PowerMockito.whenNew(JSONObject.class).withNoArguments().thenReturn(jsonObject);
+//        Mockito.when(userLoc.getLatitude()).thenReturn(0.0);
+//        Mockito.when(userLoc.getLongitude()).thenReturn(0.0);
 
-        obj = JSONFactory.userDataJSONMaker(userLoc,landmarkType);
+        // Maybe two equavilant way of writing new object construction?
+        //PowerMockito.whenNew(JSONObject.class).withNoArguments().thenReturn(jsonObject);
+        //PowerMock.expectNew(JSONObject.class).andReturn(jsonObject);
 
+        //PowerMockito.whenNew(JSONObject.class).withNoArguments().thenReturn(jsonObject);
+        PowerMockito.when(JSONFactory.userDataJSONMaker(userLoc,landmarkType)).thenReturn(jsonObject);
+        assertEquals(jsonObject, JSONFactory.userDataJSONMaker(userLoc,landmarkType));
+        assertEquals(0.0, userLoc.getLongitude(),0.1);
+        assertEquals(0.0, userLoc.getLatitude(), 0.1);
+
+        // Verify construction of object.
+        replay(jsonObject,JSONObject.class);
+        verify(jsonObject,JSONObject.class);
 
     }
 }

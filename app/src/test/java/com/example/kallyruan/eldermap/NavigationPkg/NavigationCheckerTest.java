024b69f82eb1fcba92510c39fd5f33660cb7fe24
Chengeng Liu <c.liu75@student.unimelb.edu.au>
2018-10-10 16:54:22 +1100

    No idea wtf i am writing

---
+++ app/src/test/java/com/example/kallyruan/eldermap/NavigationPkg/NavigationCheckerTest.java
@@ -0,0 +1,12 @@
+package com.example.kallyruan.eldermap.NavigationPkg;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class NavigationCheckerTest {
+
+    @Test
+    public void getPositions() {
+    }
+}
--- app/src/test/java/com/example/kallyruan/eldermap/NetworkPkg/HTTPPostRequestTest.java
+++ app/src/test/java/com/example/kallyruan/eldermap/NetworkPkg/HTTPPostRequestTest.java
@@ -1,5 +1,16 @@
 package com.example.kallyruan.eldermap.NetworkPkg;
 
+import android.os.AsyncTask;
+
+
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -19,10 +30,25 @@ import org.mockito.MockitoAnnotations;
 import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
-
+@RunWith(PowerMockRunner.class)
+@PrepareForTest({OutputStreamWriter.class, URL.class, StringBuilder.class,
+    BufferedReader.class, InputStreamReader.class, })
 public class HTTPPostRequestTest {
 
+    private String url = "HelloWorld";
+    private HTTPPostRequest request;
+    @Mock
+    private URL _url;
+    @Before
+    public void setup(){
+        //_url = PowerMockito.mock(URL.class);
+        request = new HTTPPostRequest(url);
+    }
+
     @Test
-    public void doInBackground() {
+    public void doInBackground() throws Exception{
+        URL _url = PowerMock.createMock(URL.class);
+        HttpURLConnection urlConnection = PowerMock.createMock(HttpURLConnection.class);
+        PowerMockito.whenNew(URL.class).withAnyArguments().thenReturn(_url);
     }
 }

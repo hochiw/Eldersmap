a1acd5ec3163cc53ebf0dc48045a0c3971587189
Chengeng Liu <c.liu75@student.unimelb.edu.au>
2018-10-17 14:32:30 +1100

    Basically Three classes need to rewrite tests.

---
+++ app/src/test/java/com/example/kallyruan/eldermap/GPSServicePkg/GPSTest.java
@@ -0,0 +1,61 @@
+package com.example.kallyruan.eldermap.GPSServicePkg;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+import static org.powermock.api.easymock.PowerMock.replay;
+import static org.powermock.api.easymock.PowerMock.verify;
+
+import org.mockito.BDDMockito;
+import org.powermock.api.easymock.PowerMock;
+import org.junit.Test;
+
+import org.junit.Before;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+
+public class GPSTest {
+
+    @Test
+    public void onCreate() {
+    }
+
+    @Test
+    public void onStartCommand() {
+    }
+
+    @Test
+    public void onBind() {
+    }
+
+    @Test
+    public void createLocationReq() {
+    }
+
+    @Test
+    public void onConnected() {
+    }
+
+    @Test
+    public void updateLocation() {
+    }
+
+    @Test
+    public void onConnectionSuspended() {
+    }
+
+    @Test
+    public void onConnectionFailed() {
+    }
+
+    @Test
+    public void onLocationChanged() {
+    }
+}
